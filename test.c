43331d1949bbd19f3783521db989f028ebd74f85
PikNik <numizma9@gmail.com>
2021-09-17 12:20:24 +0300

    Initial commit

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main(void) {
+	double b = 5.5;
+	long int* a = (long int*)&b;
+	printf("double was 0x%lx\n", *(long*)&b);
+
+	*a = 1111111;
+	printf("double now 0x%lx\n", *(long*)&b);
+
+	
+	printf("%lu, %lu\n", sizeof(long), sizeof(double));
+
+	printf("a = %p, &b = %p, b = %lf, *a = %ld\n", a, &b, b, *a);
+}
